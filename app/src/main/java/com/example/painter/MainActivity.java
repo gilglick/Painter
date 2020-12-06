4d5bbcffd9462cf9a701fca9147ec27571df5ca6
gilgl <gilglick@gmail.com>
2020-12-07 00:33:50 +0200

    Gif added

--- app/src/main/java/com/example/painter/MainActivity.java
+++ app/src/main/java/com/example/painter/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.painter;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -18,11 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawerHolder = findViewById(R.id.drawHolder);
-        drawerHolder.register(new DrawerHolder.ImageSavedCallback() {
-            @Override
-            public void onImageSaved(Bitmap bitmap) {
+        drawerHolder.register(bitmap -> {
 
-            }
         });
     }
 }
